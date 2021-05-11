66258ba1c684d8fb678a009d4a12190eabcc7728
Anatoliy <bashkatov.spb@mail.ru>
2021-05-11 15:32:51 +0300

    Create_programm_checkFilm

--- React JS/script.js
+++ React JS/script.js
@@ -1,3 +1,20 @@
-const category = 'toys';
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", '');
 
-console.log(`https://wh.whoIsIt?.com/${category}`)
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов', ''),
+    b = prompt('На сколько вы его оцените',''),
+    c = prompt('Один из последних просмотренных фильмов', ''),
+    d = prompt('На сколько вы его оцените','');
+
+personalMovieDB.movies.a = b;
+personalMovieDB.movies.c = d;
+
+console.log(personalMovieDB);
+
