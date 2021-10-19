eb0b9c92dd75d9783ecef0a41cfd7d396870ac0a
Anatoliy <bashkatov.spb@mail.ru>
2021-10-19 09:00:40 +0300

    gl

--- React JS/json/script.js
+++ React JS/json/script.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const options = {
+    name: 'test',
+    width: 1024,
+    height: 1024,
+    colors: {
+        border: 'black',
+        bg: 'red'
+    }
+};
+
+const arr = [1,3,4,5,6];
+
+for (let key in options) {
+    console.log(options[key]);
+}
